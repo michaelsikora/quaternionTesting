a543bb02698387521bc55c8763729f1bec3ef4cf
michaelsikora <m.sikora@uky.edu>
2018-01-14 21:08:40 -0500

    added comments for Madgwick references

--- quatRotateDup.m
+++ quatRotateDup.m
@@ -4,6 +4,7 @@ function [ vect_b ] = quatRotateDup( q, vect_a )
 %       transformation. describes reference frame b relative to a(size 1x4)
 %   vect_a - the vector in the starting reference frame a (size 1x3)
 
+% Calculation defined by Madgwick Equation 5
 % quaternion ox vecta ox quaternion* = vectb
 qconj = [q(1) -q(2) -q(3) -q(4)];
 
