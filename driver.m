1c42cca6cbf77e9a741681008ecdd55c3b769a0a
michaelsikora <m.sikora@uky.edu>
2018-01-15 17:20:01 -0500

    edits

--- driver.m
+++ driver.m
@@ -3,6 +3,7 @@
 % Quickly visualize a rotation of a unit vector in the x direction
 % using a quaternion defined by an axis and rotation
 
+<<<<<<< HEAD
 ax = [0 0 1]; % x,y,z of axis of rotation
 ax = ax./sqrt(sum(ax.^2)); % Normalizes axis to unit length
 % quatTest(pi/2, ax); % rotates and plots
@@ -18,3 +19,20 @@ for ii = 1:N
     quatTest(2*ii*pi/N, ax);
     pause(0.02);
 end
+=======
+ax = [0 1 0]; % x,y,z of axis of rotation
+ax = ax./sqrt(sum(ax.^2)); % Normalizes axis to unit length
+% quatTest(pi/2, ax); % rotates and plots
+
+quatTest(pi/4, ax);
+
+
+
+% % full 360 degree rotation about the -y axis
+% ax = [0 -1 0]; ax = ax./sqrt(sum(ax.^2));
+% N = 10;
+% for ii = 1:N
+%     quatTest(2*ii*pi/N, ax);
+%     pause(0.02);
+% end
+>>>>>>> 41fb5175e58dce411360851430e3082578740068
